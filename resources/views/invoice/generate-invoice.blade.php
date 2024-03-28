aa93f80271ba4bbca4df1324a660d127e7503099
chetanis <anis.chetouane@etu.usthb.dz>
2024-03-28 21:18:23 +0100

    edited the invoice

--- resources/views/invoice/generate-invoice.blade.php
+++ resources/views/invoice/generate-invoice.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Invoice #6</title>
+    <title>facture N° {{ $sale->id }}-{{ $sale->created_at->format('Y') }}</title>
 
     <style>
         html,
@@ -11,21 +12,35 @@
             padding: 10px;
             font-family: sans-serif;
         }
-        h1,h2,h3,h4,h5,h6,p,span,label {
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        span,
+        label {
             font-family: sans-serif;
         }
+
         table {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 0px !important;
         }
+
         table thead th {
             height: 28px;
             text-align: left;
             font-size: 16px;
             font-family: sans-serif;
         }
-        table, th, td {
+
+        table,
+        th,
+        td {
             border: 1px solid #ddd;
             padding: 8px;
             font-size: 14px;
@@ -37,31 +52,42 @@
             margin-bottom: 12px;
             font-family: sans-serif;
         }
+
         .small-heading {
             font-size: 18px;
             font-family: sans-serif;
         }
+
         .total-heading {
             font-size: 18px;
             font-weight: 700;
             font-family: sans-serif;
         }
+
         .order-details tbody tr td:nth-child(1) {
-            width: 20%;
+            width: 23%;
         }
+
         .order-details tbody tr td:nth-child(3) {
-            width: 20%;
+            width: 23%;
         }
 
         .text-start {
             text-align: left;
         }
+        .company{
+            text-align: left;
+            margin-left: 10px;
+        }
+
         .text-end {
             text-align: right;
         }
+
         .text-center {
             text-align: center;
         }
+
         .company-data span {
             margin-bottom: 4px;
             display: inline-block;
@@ -69,70 +95,88 @@
             font-size: 14px;
             font-weight: 400;
         }
+
         .no-border {
             border: 1px solid #fff !important;
         }
+
         .bg-blue {
             background-color: #414ab1;
             color: #fff;
         }
+
+        .w-top-bor{
+            border-top: 1px solid white ;
+        }
+        .w-left-bor{
+            border-left: 1px solid white ;
+        }
+        .w-bot-bor{
+            border-bottom: 1px solid white ;
+        }
+        .w-right-bor{
+            border-right: 1px solid white ;
+        }
     </style>
 </head>
+
 <body>
+    <?php
+    $command = $sale->command;
+    $client = $command->client;
+    $productsWithQuantities = $command->productsAndQuantities;
+    $tva = round(0.19 * $command->total_price, 2);
+    $timbre = round(0.01 * $command->total_price, 2);
+    $total = $command->total_price + $tva + $timbre;
+    ?>
 
     <table class="order-details">
         <thead>
             <tr>
                 <th width="50%" colspan="2">
-                    <h2 class="text-start">Funda Ecommerce</h2>
+                    <h2 class="text-start company">Nom société</h2>
                 </th>
                 <th width="50%" colspan="2" class="text-end company-data">
-                    <span>Invoice Id: #6</span> <br>
-                    <span>Date: 24 / 09 / 2022</span> <br>
-                    <span>Zip code : 560077</span> <br>
-                    <span>Address: #555, Main road, shivaji nagar, Bangalore, India</span> <br>
+                    <span>Facture N° {{ $sale->id }}</span> <br>
+                    <span>Date: {{ $sale->created_at->format('d/m/Y') }}</span> <br>
+                    <span>Adresse: 14,chemin Mohamed Ali, Biskra</span> <br>
+                    <span>Zip code : 16000</span> <br>
+                    <span>Téléphone: 02323232323</span> <br>
                 </th>
             </tr>
             <tr class="bg-blue">
-                <th width="50%" colspan="2">Order Details</th>
-                <th width="50%" colspan="2">User Details</th>
+                <th width="50%" colspan="2">Détails de la commande</th>
+                <th width="50%" colspan="2">Détails de client</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>Order Id:</td>
-                <td>6</td>
+                <td>Commande N°:</td>
+                <td>{{ $command->id }}</td>
 
-                <td>Full Name:</td>
-                <td>Ved Prakash</td>
+                <td>Nom complet</td>
+                <td>{{ $client->last_name }} {{ $client->first_name }}</td>
             </tr>
             <tr>
-                <td>Tracking Id/No.:</td>
-                <td>funda-CRheOqspbA</td>
+                <td>Date de commande:</td>
+                <td>{{ $command->created_at->format('d/m/Y \à H:i') }}</td>
 
-                <td>Email Id:</td>
-                <td>ved@gmail.com</td>
+                <td>Email:</td>
+                <td>{{ $client->email }}</td>
             </tr>
             <tr>
-                <td>Ordered Date:</td>
-                <td>22-09-2022 10:54 AM</td>
-
-                <td>Phone:</td>
-                <td>8889997775</td>
-            </tr>
-            <tr>
-                <td>Payment Mode:</td>
+                <td>Méthode de paiement:</td>
                 <td>Cash on Delivery</td>
 
-                <td>Address:</td>
-                <td>asda asdad asdad adsasd</td>
+                <td>Téléphone:</td>
+                <td>{{ $client->phone_number }}</td>
             </tr>
             <tr>
-                <td>Order Status:</td>
-                <td>completed</td>
+                <td>Etat de la commande:</td>
+                <td>Confirmé</td>
 
-                <td>Pin code:</td>
-                <td>566999</td>
+                <td>Adresse:</td>
+                <td>{{ $client->address }}</td>
             </tr>
         </tbody>
     </table>
@@ -141,47 +185,56 @@
         <thead>
             <tr>
                 <th class="no-border text-start heading" colspan="5">
-                    Order Items
+                    Liste des produits
                 </th>
             </tr>
             <tr class="bg-blue">
                 <th>ID</th>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
+                <th>Produit</th>
+                <th>PU HT</th>
+                <th>Quantité</th>
+                <th>Montant HT</th>
             </tr>
         </thead>
         <tbody>
+            @foreach ($productsWithQuantities as $productWithQuantity)
+                <tr>
+                    <td width="10%">{{ $loop->iteration }}</td>
+                    <td>{{ $productWithQuantity['product']->name }}</td>
+                    <td width="15%">${{ $productWithQuantity['product']->price }}</td>
+                    <td width="20%">{{ $productWithQuantity['quantity'] }}</td>
+                    <td width="20%" class="fw-bold">
+                        ${{ $productWithQuantity['product']->price * $productWithQuantity['quantity'] }}</td>
+                </tr>
+            @endforeach
             <tr>
-                <td width="10%">16</td>
-                <td>
-                    Mi Note 7
-                </td>
-                <td width="10%">$14000</td>
-                <td width="10%">1</td>
-                <td width="15%" class="fw-bold">$14000</td>
+                <td colspan="3" class="w-top-bor w-bot-bor w-left-bor "></td>
+                <td colspan="1">DZD HT:</td>
+                <td colspan="1">{{ $command->total_price }}</td>
             </tr>
             <tr>
-                <td width="10%">17</td>
-                <td>
-                    Vivo V19
-                </td>
-                <td width="10%">$699</td>
-                <td width="10%">1</td>
-                <td width="15%" class="fw-bold">$699</td>
+                <td colspan="3" class=" w-top-bor w-bot-bor w-left-bor"></td>
+                <td colspan="1">TVA 19%:</td>
+                <td colspan="1">{{ $tva }}</td>
             </tr>
             <tr>
-                <td colspan="4" class="total-heading">Total Amount - <small>Inc. all vat/tax</small> :</td>
-                <td colspan="1" class="total-heading">$14699</td>
+                <td colspan="2" class="w-top-bor w-bot-bor w-left-bor w-right-bor"></td>
+                <td colspan="1" class="w-left-bor "></td>
+                <td colspan="1">Droit de timbre 1%:</td>
+                <td colspan="1">{{ $timbre }}</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="w-top-bor w-bot-bor w-left-bor"></td>
+                <td colspan="1" class="total-heading">TOTAL DZD</td>
+                <td colspan="1" class="total-heading">TTC</td>
+                <td colspan="1" class="total-heading">{{ $total }}</td>
             </tr>
         </tbody>
     </table>
 
     <br>
-    <p class="text-center">
-        Thank your for shopping with Funda of Web IT
-    </p>
+    <p >Mode de règlement: espèce</p>
 
 </body>
+
 </html>
