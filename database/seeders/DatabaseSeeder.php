970f60668e35573f37c499a9401b35ef60874552
chetanis <anis.chetouane@etu.usthb.dz>
2024-04-04 15:11:40 +0100

    add seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\User;
-use Faker\Factory;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        User::factory(10)->create();
+        $users = [
+            [
+                'username' => 'admin',
+                'password' => Hash::make('12345678'),
+                'privilege' => 'admin',
+            ],
+            [
+                'username' => 'superuser',
+                'password' => Hash::make('12345678'),
+                'privilege' => 'superuser',
+            ],
+            [
+                'username' => 'user',
+                'password' => Hash::make('12345678'),
+                'privilege' => 'user',
+            ],
+            // Add more users as needed
+        ];
+
+        // Insert the users into the database
+        DB::table('users')->insert($users);
 
         
     }
