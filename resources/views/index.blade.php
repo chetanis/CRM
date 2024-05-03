1123a7a5d9d79a0c0e015418761ae63d08298014
chetanis <anis.chetouane@etu.usthb.dz>
2024-05-03 15:35:14 +0100

    change home page UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -227,6 +227,48 @@
                             </div>
                         </div><!-- End Recent Sales -->
 
+                    </div>
+                </div><!-- End Left side columns -->
+
+                <!-- Right side columns -->
+                <div class="col-lg-4">
+
+                    <!-- Appointments -->
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Rendez-vous d'aujourd'hui</h5>
+                            <div class="activity">
+                                @if ($appointments->count() == 0)
+                                    <h4>pas de rendez vous</h4>
+                                @else
+                                    @foreach ($appointments as $appointment)
+                                        <div class="activity-item d-flex">
+                                            <div class="activite-label">{{ $appointment->getHour() }}</div>
+                                            @if ($appointment->status == 'done')
+                                                <i
+                                                    class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
+                                            @elseif ($appointment->status == 'pending')
+                                                <i
+                                                    class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
+                                            @else
+                                                <i
+                                                    class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
+                                            @endif
+                                            <div class="activity-content">
+                                                <a href="/appointments/{{ $appointment->id }}" class="text-dark">
+                                                    Rendez-vous avec <span
+                                                        class="fw-bold text-primary">{{ $appointment->client->first_name . $appointment->client->last_name }}</span></a>
+                                            </div>
+                                        </div><!-- End activity item-->
+                                    @endforeach
+                                @endif
+
+                            </div>
+
+                        </div>
+                    </div><!-- End Recent Activity -->
+                    <!-- if admin -->
+                    @if (Auth::user()->privilege == 'admin')
                         <!-- Top Selling -->
                         <div class="col-12">
                             <div class="card top-selling overflow-auto">
@@ -251,9 +293,9 @@
                                     <table class="table table-borderless">
                                         <thead>
                                             <tr>
-                                                <th scope="col">#</th>
+                                                {{-- <th scope="col">#</th> --}}
                                                 <th scope="col">Product</th>
-                                                <th scope="col">Price</th>
+                                                {{-- <th scope="col">Price</th> --}}
                                                 <th scope="col">Sold</th>
                                                 <th scope="col">Revenue</th>
                                             </tr>
@@ -261,10 +303,10 @@
                                         <tbody>
                                             @foreach ($topProducts as $product)
                                                 <tr>
-                                                    <th scope="row" class="text-primary">{{ $product->id }}</th>
+                                                    {{-- <th scope="row" class="text-primary">{{ $product->id }}</th> --}}
                                                     <td><a href='/products/{{ $product->id }}'
                                                             class="text-primary fw-bold">{{ $product->name }}</a></td>
-                                                    <td>{{ $product->price }} DA</td>
+                                                    {{-- <td>{{ $product->price }} DA</td> --}}
                                                     <td class="fw-bold">{{ $product->sold }}</td>
                                                     <td>{{ $product->sold * $product->price }} DA</td>
                                                 </tr>
@@ -276,101 +318,7 @@
 
                             </div>
                         </div><!-- End Top Selling -->
-
-                    </div>
-                </div><!-- End Left side columns -->
-
-                <!-- Right side columns -->
-                <div class="col-lg-4">
-
-                    <!-- Recent Activity -->
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Rendez-vous d'aujourd'hui</h5>
-                            <div class="activity">
-                                @if ($appointments->count() == 0)
-                                    <h4>pas de rendez vous</h4>
-                                @else
-                                    @foreach ($appointments as $appointment)
-                                        <div class="activity-item d-flex">
-                                            <div class="activite-label">{{ $appointment->getHour() }}</div>
-                                            @if ($appointment->status == 'done')
-                                                <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
-                                            @elseif ($appointment->status == 'pending')
-                                                <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
-                                            @else
-                                                <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
-                                            @endif
-                                            <div class="activity-content">
-                                                <a href="/appointments/{{ $appointment->id }}" class="text-dark"> Rendez-vous avec <span class="fw-bold text-primary">{{$appointment->client->first_name . $appointment->client->last_name}}</span></a>
-                                            </div>
-                                        </div><!-- End activity item-->
-                                    @endforeach
-                                @endif
-
-                            </div>
-
-                        </div>
-                    </div><!-- End Recent Activity -->
-
-                    <!-- News & Updates Traffic -->
-                    <div class="card">
-                        <div class="filter">
-                            <a class="icon" href="#" data-bs-toggle="dropdown"><i
-                                    class="bi bi-three-dots"></i></a>
-                            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                <li class="dropdown-header text-start">
-                                    <h6>Filter</h6>
-                                </li>
-
-                                <li><a class="dropdown-item" href="#">Today</a></li>
-                                <li><a class="dropdown-item" href="#">This Month</a></li>
-                                <li><a class="dropdown-item" href="#">This Year</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="card-body pb-0">
-                            <h5 class="card-title">News &amp; Updates <span>| Today</span></h5>
-
-                            <div class="news">
-                                <div class="post-item clearfix">
-                                    <img src="assets/img/news-1.jpg" alt="">
-                                    <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
-                                    <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
-                                </div>
-
-                                <div class="post-item clearfix">
-                                    <img src="assets/img/news-2.jpg" alt="">
-                                    <h4><a href="#">Quidem autem et impedit</a></h4>
-                                    <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...
-                                    </p>
-                                </div>
-
-                                <div class="post-item clearfix">
-                                    <img src="assets/img/news-3.jpg" alt="">
-                                    <h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
-                                    <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...
-                                    </p>
-                                </div>
-
-                                <div class="post-item clearfix">
-                                    <img src="assets/img/news-4.jpg" alt="">
-                                    <h4><a href="#">Laborum corporis quo dara net para</a></h4>
-                                    <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...
-                                    </p>
-                                </div>
-
-                                <div class="post-item clearfix">
-                                    <img src="assets/img/news-5.jpg" alt="">
-                                    <h4><a href="#">Et dolores corrupti quae illo quod dolor</a></h4>
-                                    <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos
-                                        eius...</p>
-                                </div>
-
-                            </div><!-- End sidebar recent posts-->
-
-                        </div>
-                    </div><!-- End News & Updates -->
+                    @endif
 
                 </div><!-- End Right side columns -->
 
