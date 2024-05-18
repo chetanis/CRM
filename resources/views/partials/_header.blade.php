8563b9e860b01d198f1d8a13d801137dc19f14f3
chetanis <anis.chetouane@etu.usthb.dz>
2024-05-18 21:44:35 +0100

    make the notifications scrollable

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -21,7 +21,7 @@
         <span id="notifCount" class="badge bg-danger badge-number"></span>
       </a><!-- End Notification Icon -->
 
-      <ul id="notifications" class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+      <ul id="notifications" class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications" style="max-height: 300px;overflow-y: auto;">
         <li id="nb_notif" class="dropdown-header">
           You have 0 new notifications
         </li>
